6edee16cb61de3d70d1fb5f133a6fda25301e3f6
vamsi <bathuyuvaraj@gmail.com>
2026-08-13 12:17:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Balanced Array/balanced-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minValueToBalance(List<Integer> arr) {
+        // code here
+        int n = arr.size();
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            leftSum += arr.get(i);
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            rightSum += arr.get(i);
+        }
+
+        return Math.abs(leftSum - rightSum);
+    }
+};
