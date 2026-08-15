bf5d986525142a00c8a8233e9e1d1e9e857d33b1
vamsi <bathuyuvaraj@gmail.com>
2026-08-15 11:59:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Prefix Maximums/count-prefix-maximums.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countElements(int[] arr) {
+        // code here
+        int count = 1;
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                count++;
+                max = arr[i];
+            }
+        }
+
+        return count;
+    }
+}
